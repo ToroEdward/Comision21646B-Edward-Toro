d7943a92e5df2174f554a89cc34e0192075ef52b
Edward <edwardtoro688@gmail.com>
2023-09-26 20:26:42 -0300

    se crearon las carpeta middlewares y models/Schemas

---
+++ src/models/Schemas/post.schema.js
@@ -0,0 +1,28 @@
+import { body } from "express-validator";
+
+export const createPostSchema = [
+    body("tilte")
+        .isString().withMessage("Debe ser string")
+        .notEmpty().withMessage("No debe ser vacio"),
+    body("description")
+        .isString().withMessage("Debe ser string")
+        .notEmpty().withMessage("No debe ser vacio"),
+    body("LinkUrl")
+        .isURL().withMessage("Ingrese una url valida")
+        .notEmpty().withMessage("No debe ser vacio"),
+]
+
+export const editPostSchema = [
+    body("tilte")
+        .optional()
+        .isString().withMessage("Debe ser string")
+        .notEmpty().withMessage("No debe ser vacio"),
+    body("description")
+        .optional()
+        .isString().withMessage("Debe ser string")
+        .notEmpty().withMessage("No debe ser vacio"),
+    body("LinkUrl")
+        .optional()
+        .isURL().withMessage("Ingrese una url valida")
+        .notEmpty().withMessage("No debe ser vacio"),
+]
