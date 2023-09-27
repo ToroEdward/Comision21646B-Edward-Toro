cd3a9805753fd5f5b42f60e0651edb366e5ee6d7
Edward <edwardtoro688@gmail.com>
2023-09-27 20:03:57 -0300

    se modificaron los siguientes archivos: app.js, package.json, Posts.ejs y footer.ejs

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import { postRouter } from './src/routes/post.routes.js';
 import { startDb } from './src/config/database.js';
 import path from 'node:path'
 import cors from 'cors'
+import morgan from 'morgan';
+import helmet from 'helmet'
 import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url)
@@ -13,6 +15,10 @@ const app = express();
 // middlewares
 app.use(express.json())
 app.use(cors())
+app.use(morgan('dev'))
+app.use(helmet({
+    contentSecurityPolicy: false
+}))
 
 app.use(express.static(path.join(__dirname, "src", "public")))
 
