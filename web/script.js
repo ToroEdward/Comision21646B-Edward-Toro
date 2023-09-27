94de3b4679f9b8a0b9a39fd86e3bc61b597cc021
Edward <edwardtoro688@gmail.com>
2023-09-27 01:26:23 -0300

    se reslizaron cambios en los archivos de la carpeta web

---
+++ web/script.js
@@ -0,0 +1,132 @@
+const contenedor = document.getElementById('container-row');
+const btnCrear = document.getElementById('btn-new');
+const myModal = new bootstrap.Modal(document.getElementById('myModal'));
+const btnSave = document.getElementById('btn-save');
+const form = document.getElementById('formulario');
+// console.log(contenedor);
+
+let html = "";
+let option = "";
+let idForm = "";
+
+const inputTitle = document.getElementById('inputTitle')
+const inputDescription = document.getElementById('inputDescription')
+const inputLinkUrl = document.getElementById('inputLinkUrl')
+
+btnCrear.addEventListener('click', () => {
+    option = 'new'
+    btnSave.textContent = 'new'
+    inputTitle.value = ''
+    inputDescription.value = ''
+    inputLinkUrl.value = ''
+    myModal.show()
+})
+
+document.addEventListener('click', (event) => {
+    if (event.target.matches('#btn-delete')){
+        const article = event.target.closest('.col-4')
+        const idArticle = article.dataset.id
+
+        fetch(`http://localhost:3001/api/posts/${idArticle}`, {
+            method: 'DELETE'
+        }).then(res => {
+            if(res.ok) {
+                article.remove()
+            }
+        }).catch(err => {
+            console.err(err)
+        })
+    }
+})
+
+document.addEventListener('click', (event) => {
+    if (event.target.matches('#btn-edit')) {
+        const article = event.target.closest('.col-4')
+
+        const idArticle = article.dataset.id
+        const urlLinkUrlEdit = article.children[0].children[0].src;
+        const tilteEdit = article.children[0].children[1].children[0].textContent;
+        const descriptionEdit = article.children[0].children[1].children[1].textContent;
+
+        idForm = idArticle;
+        inputTitle.value = tilteEdit;
+        inputDescription.value = descriptionEdit;
+        inputLinkUrl.value = urlLinkUrlEdit;
+        option = 'edit';
+        btnSave.textContent = 'Edit';
+        myModal.show();
+    }
+});
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    if (option === "new") {
+        const newPost = {
+            title: inputTitle.value,
+            description: inputDescription.value,
+            linkUrl: inputLinkUrl.value,
+        };
+
+        fetch('http://localhost:3001/api/posts', {
+            method: 'POST',
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(newPost),
+        }).then((res) => {
+            if(res.ok) {
+                alert("Posteo creado exitosamente");
+                myModal.hide();
+                location.reload();
+            }
+        });
+    }
+
+    if (option === "edit") {
+        const newPost = {
+            title: inputTitle.value,
+            description: inputDescription.value,
+            linkUrl: inputLinkUrl.value,
+        };
+
+        fetch(`http://localhost:3001/api/posts/${idForm}`, {
+            method: 'PUT',
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(newPost)
+        }).then((res) => {
+            if(res.ok) {
+                alert("Posteo editado exitosamente")
+                myModal.hide();
+                location.reload();
+            }
+        })
+    }
+});
+
+fetch('http://localhost:3001/api/posts')
+    .then((res) => res.json())
+    .then((data) => {
+        // console.log(data);
+        data.forEach((post) => {
+            html += `
+            <article class="col-4 d-flex justify-content-center mb-3" data-id=${post.id}>
+                <div class="card" style="width: 18rem;">
+                    <img src="${post.linkUrl}">
+                    <div class="card-body">
+                        <h5 class="card-title">${post.title}</h5>
+                        <p class="card-text">${post.description}</p>
+                        <div>
+                            <button class="btn btn-info" id="btn-edit">Editar</button>
+                            <button type="" class="btn btn-danger" id="btn-delete">Eliminar</button>
+                        </div>
+                    </div>
+                </div>
+            </article>
+            `
+
+            contenedor.innerHTML = html;
+        });
+    })
